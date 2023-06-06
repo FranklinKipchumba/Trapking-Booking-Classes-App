2ffb77a4e22f8a39fdc892c07288eb95d96f82b3
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-06-06 23:15:44 +0300

    API under development

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,36 @@
 class SchedulesController < ApplicationController
+    def index 
+        schedules = Schedule.all
+        render json: schedules
+    end
+    def show
+        schedule = Schedule.find(params[:id])
+        render json: schedule
+    end
+    def create
+        schedule = Schedule.new(schedule_params)
+        if schedule.save
+            render json: schedule, status: :created
+        else 
+            render json: { errors: schedule.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    def update
+        schedule = Schedule.find(params[:id])
+        if schedule.update(schedule_params)
+            render json: schedule, status: :ok
+        else
+            render json: { errors: schedule.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    def destroy
+        schedule = Schedule.find(params[:id])
+        schedule.destroy
+        head :no_content
+    end
+
+    private
+    def schedule_params
+        params.require(:schedule).permit(:class_id, :start_time, :end_time, :availability)
+    end
 end
