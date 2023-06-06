01a7c7d8016203fc07ebb852cd06aaf07bc335bb
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-06-06 23:07:26 +0300

    API under development

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,36 @@
 class BookingsController < ApplicationController
+    skip_before_action :authorize_request, only: [:show, :update, :destroy, :create]
+
+    def index
+        bookings = Booking.all 
+        render json: bookings
+    end
+    def show 
+        booking = Booking.find(params[:id])
+        render json: booking
+    end
+    def create
+        booking = Booking.new(booking_params)
+        if booking.save
+            render json: booking, status: :created, location: booking
+        else 
+            render json: {errors: booking.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+    def update
+        if booking.update(booking_params)
+            render json: booking
+        else 
+            render json: {errors: booking.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+    def destroy
+        booking.destroy
+        render json: { booking: 'Booking was successfully destroyed.' }
+    end
+    
+    private
+    def booking_params
+        params.require(:booking).permit(:user_id, :class_id, :time, :date)
+    end
 end
--- app/controllers/dance_classes_controller.rb
+++ app/controllers/dance_classes_controller.rb
@@ -1,2 +1,35 @@
 class DanceClassesController < ApplicationController
+    def index
+        danceclasses = DanceClass.all
+        render json: danceclasses 
+    end
+    def show
+        danceclass = DanceClass.find(params[:id])
+        render json: danceclass 
+    end
+    def create
+        if danceclass.save
+            render json: danceclass, status: :created
+        else
+            render json: {errors: danceclass.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+    def update
+        danceclass = DanceClass.find(params[:id])
+        if danceclass.update(danceclass_params)
+            render json: danceclass, status: :ok
+        else
+            render json: {errors: danceclass.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+    def destroy 
+        danceclass = DanceClass.find(params[:id])
+        danceclass.destroy
+        head :no_content
+    end
+
+    private
+    def danceclass_params
+        params.require(:danceclass).permit(:classname, :description, :duration, :capacity)
+    end
 end
